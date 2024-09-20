f335713341e376dcc4aca7601e3f4c6f6b31c42e
commercialgym <159087491+commercialgym@users.noreply.github.com>
2024-09-20 17:33:56 -0400

    Creating file
    creating a file and populating it and experimenting with the functions

--- assignment1.cpp
+++ assignment1.cpp
@@ -6,7 +6,7 @@
 #include <string.h>
 #pragma warning(disable:4996)
 #define MAX_CHARS 25
-//#define ITEM_SIZE (sizeof(int) + NAME_SIZE + CATEGORY_SIZE + sizeof(int) + sizeof(float)) //to be used for accessing the index of a file 
+#define ITEM_SIZE (sizeof(int) + NAME_SIZE + CATEGORY_SIZE + sizeof(int) + sizeof(float)) //to be used for accessing the index of a file
 
 long indexFilePosition(int productID);
 
@@ -20,7 +20,7 @@ int main(void)
         float price;
     }StoreItems;
 
-    FILE* pFile = fopen("StoreCatalog", "wb+");
+    FILE* pFile = fopen("StoreCatalog.dat", "wb+"); //change to rb+ for when actually coding the program
     if (pFile == NULL)
     {
         printf("Unable to open file\n");
@@ -30,10 +30,25 @@ int main(void)
     //might write an arbitrary amount of data to the file initially to use later for read, update, delete
 
     StoreItems fileData = { 1, "Smartphone", "Electronics", 50, 899.99 };
+    StoreItems fileData2 = { 2, "Smartphone", "Electronics", 30, 799.99 };
+    StoreItems fileData3 = { 3, "Laptop", "Electronics", 30, 599.99 };
+    StoreItems fileData4 = { 4, "Apples", "Produce", 110, 4.99 };
 
 
     fwrite(&fileData, sizeof(StoreItems), 1, pFile);
+    fwrite(&fileData2, sizeof(StoreItems), 1, pFile);
+    fwrite(&fileData3, sizeof(StoreItems), 1, pFile);
+    fwrite(&fileData4, sizeof(StoreItems), 1, pFile);
 
+    StoreItems readItem;
+    fseek(pFile, sizeof(StoreItems) * 2, SEEK_SET);
+    fread(&readItem, sizeof(StoreItems), 1, pFile);
+    printf("%d, %s, %s, %d, %f\n", readItem.productID, readItem.name, readItem.category, readItem.quantity, readItem.price);
+
+    if (fclose(pFile) == EOF) {
+        printf("Error closing file\n\n");
+        clearerr(pFile);
+    }
 
     return 0;
 }
@@ -44,7 +59,7 @@ int main(void)
 //RETURNS: 
 long indexFilePosition(int productID)
 {
-
+    return 0;
 }
 
 //NAME:
