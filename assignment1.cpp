4263a35e7171891de48bd5ad68e9e4f1e7b54df5
commercialgym <159087491+commercialgym@users.noreply.github.com>
2024-09-19 22:18:27 -0400

    innit commit
    created a struct with fields from the example data given in requirements

---
+++ assignment1.cpp
@@ -0,0 +1,24 @@
+// assignment1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#pragma warning(disable:4996)
+#define MAX_CHARS 25
+
+int main(void)
+{
+    typedef struct {
+        int productID;
+        char name[MAX_CHARS];
+        char category[MAX_CHARS];
+        int quantity;
+        float price;
+    }StoreItems;
+
+
+
+    return 0;
+}
+
