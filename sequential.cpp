819f3aedbe4b5d124601c130b8c3ab8ecf433ecd
commercialgym <159087491+commercialgym@users.noreply.github.com>
2024-09-22 19:00:50 -0400

    adding a new element to the list
    had to play around with realloc since i havent used it that often.

--- sequential.cpp
+++ sequential.cpp
@@ -8,6 +8,8 @@
 #pragma warning(disable:4996)
 #define MAX_CHARS 26
 #define VALID_INPUT 1
+#define SCND_DECIMAL 100
+#define BUFF_SIZE 100
 
 typedef struct {
 	int productID;
@@ -21,7 +23,8 @@ long CalculateFileSize(FILE* pFile);
 int NumElements(FILE* pFile);
 bool ReadDataFromFile(FILE* pFile, StoreItems **productCatalog, int count);
 void PrintDataFromMemory(StoreItems** productCatalog, int counter);
-
+void AssignProductInfo(StoreItems* newProduct); //from user
+void AddProductToList(StoreItems** productCatalog, StoreItems* newProduct, int* numProduct); //adds to list in memory
 
 int main(void)
 {
@@ -36,11 +39,8 @@ int main(void)
 
 	int numProducts = NumElements(pFile);
 
-	ReadDataFromFile(pFile, &productCatalog, numProducts);
-
-	PrintDataFromMemory(&productCatalog, numProducts);
-
 	//read the data from the file sequentially
+	ReadDataFromFile(pFile, &productCatalog, numProducts);
 
 	if (fclose(pFile) == EOF) {
 		printf("Error closing file\n\n");
@@ -48,11 +48,15 @@ int main(void)
 	}
 
 		//and print the data that is now held in memory
+	PrintDataFromMemory(&productCatalog, numProducts);
 
 	//update the data and write the entire set back to the file in sequence
 		//maybe take user input?
 
 		//add a new product
+	StoreItems newProduct = { 0, "Deleted", "Deleted", 0, 0 };
+	AssignProductInfo(&newProduct);
+
 
 		//update a product
 
@@ -138,4 +142,104 @@ void PrintDataFromMemory(StoreItems** productCatalog, int counter)
 		printf("Quantity: %d\n", (*productCatalog)[index].quantity);
 		printf("Price: %.2f\n", (*productCatalog)[index].price);
 	}
+}
+
+void AssignProductInfo(StoreItems* newProduct)
+{
+	char newName[MAX_CHARS] = { 0 };
+	char newCategory[MAX_CHARS] = { 0 };
+	int newQuantity = 0;
+	float newPrice = 0;
+	char buffer[BUFF_SIZE] = { 0 }; //does it have to be 100 magic number
+
+	while (getchar() != '\n'); //to ensure that the buffer is cleared. when this wasn't here the program would skip taking input for the new name
+	while (true)
+	{
+		printf("Enter the name of the new product: ");
+		if (fgets(buffer, sizeof(buffer), stdin) != NULL)
+		{
+			buffer[strcspn(buffer, "\n")] = 0;
+			if (strlen(buffer) >= MAX_CHARS)
+			{
+				printf("Invalid Input: Please enter a name 25 characters or less\n");
+			}
+			else
+			{
+				strcpy(newName, buffer);
+				break;
+			}
+		}
+	}
+
+	while (true)
+	{
+		printf("Enter the category of the new product: ");
+		if (fgets(buffer, sizeof(buffer), stdin) != NULL)
+		{
+			buffer[strcspn(buffer, "\n")] = 0;
+			if (strlen(buffer) >= MAX_CHARS)
+			{
+				printf("Invalid Input: Please enter a name 25 characters or less\n");
+			}
+			else
+			{
+				strcpy(newCategory, buffer);
+				break;
+			}
+		}
+	}
+
+	while (true)
+	{
+		printf("Enter the quantity: ");
+		if (scanf("%d", &newQuantity) != VALID_INPUT)
+		{
+			printf("Invalid Input: Please enter an integer value\n");
+			while (getchar() != '\n');
+			continue;
+		}
+		else
+		{
+			while (getchar() != '\n');
+			break;
+		}
+	}
+
+	while (true)
+	{
+		printf("Enter the price: ");
+		if (scanf("%f", &newPrice) != VALID_INPUT)
+		{
+			printf("Invalid Input: Please enter a float value\n");
+			while (getchar() != '\n');
+			continue;
+		}
+		else
+		{
+			while (getchar() != '\n');
+			break;
+		}
+	}
+	//add to struct
+	newProduct->productID = 0;
+	strcpy(newProduct->name, newName);
+	strcpy(newProduct->category, newCategory);
+	newProduct->quantity = newQuantity;
+	newPrice = (float)(round(newPrice * SCND_DECIMAL) / SCND_DECIMAL);
+	newProduct->price = newPrice;
+}
+
+void AddProductToList(StoreItems** productCatalog, StoreItems* newProduct, int* numProducts)
+{
+	StoreItems* tempCatalog = (StoreItems*)realloc(*productCatalog, (*numProducts + 1) * sizeof(StoreItems));
+	if (tempCatalog == NULL)
+	{
+		printf("Error Reallocating Memory\n");
+		return;
+	}
+	*productCatalog = tempCatalog;
+	*numProducts += 1; //increment since we're adding a new element to the list
+	newProduct->productID = *numProducts; //assign an id so it's not 0 and marked as deleted when adding back to file
+	(*productCatalog)[*numProducts] = *newProduct;
+
 }
